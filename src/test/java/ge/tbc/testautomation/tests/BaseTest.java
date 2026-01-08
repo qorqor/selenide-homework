313e5d05bb7b0735b5fe4d3eb1038a02839872b3
0kazi <g.kazishvili2@gmail.com>
2026-01-08 15:11:01 +0400

    TestNG group and Retry

--- src/test/java/ge/tbc/testautomation/tests/BaseTest.java
+++ src/test/java/ge/tbc/testautomation/tests/BaseTest.java
@@ -13,7 +13,7 @@ public class BaseTest {
 
     @BeforeTest
     public void beforeTest(){
-        System.out.println("Browser started working!");
+        System.out.println("BROWSER DID NOT START WORKING!");
     }
 
     @AfterTest
